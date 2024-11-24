77b1c4b9e07aad46f952a05c657863f3e68f34ab
percyjavier1977 <javierizquierdopajuelo@gmail.com>
2024-11-23 22:06:18 -0500

    javaScript

---
+++ ejemplo.js
@@ -0,0 +1,2 @@
+console.log("Hola bienvenido");
+console.log("Clase de JavaScript");
---
+++ ejemplo2.js
@@ -0,0 +1,21 @@
+let n1 = 5;
+let n2 = 4;
+
+//Suma
+let suma = n1 + n2;
+
+//Resta
+let resta = n1 - n2;
+
+//Multiplicación
+
+let multi = n1 * n2;
+
+//División
+
+let divi = n1 / n2;
+
+console.log("La suma es: " + suma);
+console.log("La resta es: " + resta);
+console.log("la multiplicación es: " + multi);
+console.log("la división es: " + divi)
