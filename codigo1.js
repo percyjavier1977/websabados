fb3e97852a29c28a275c042a10b145a62b069832
percyjavier1977 <javierizquierdopajuelo@gmail.com>
2024-12-14 22:11:43 -0500

    JavaScript 14-12

---
+++ codigo1.js
@@ -0,0 +1,5 @@
+document.getElementById('titulo1').textContent = "Cambio título 1";
+document.getElementById('titulo2').textContent = "Cambio título 2";
+document.getElementById('parrafo1').textContent = "Cambio parrafo 1";
+document.getElementById('parrafo2').textContent = "Cambio parrafo 2";
+
---
+++ script2.js
@@ -0,0 +1,2 @@
+document.getElementById('header1').textContent="Texto cambiado 1";
+document.getElementById('header2').textContent="Texto cambiado 2";
